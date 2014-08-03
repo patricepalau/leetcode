dac291c9853081adc646d37c3639296f3e74b86c
Patrice <patrice147@gmail.com>
2014-08-03 21:58:18 +0200

    solution for 3sum problem

--- src/threesum/Solution.java
+++ src/threesum/Solution.java
@@ -1,8 +1,13 @@
 package threesum;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 
@@ -14,53 +19,133 @@ import java.util.List;
  * - Elements in a triplet (a,b,c) must be in non-descending order. (ie, a <= b <= c)
  * - The solution set must not contain duplicate triplets.
  * 
- * THIS SOLUTION IS TOO SLOW - NOT ACCEPTED
+ * The following solution, based on the solution for 2-sum, got accepted:
+ * - for every item a in the array, apply 2 sum on the remaining array, searching for (target-a)
+ * 
+ * Solution accepted after a few changes:
+ * - use of primitive arrays and binary search
+ * - result cache for twoSum
+ * - also moved collection of results out of twosum out of the main loop
+ * 
  */
 public class Solution {
-	private List<List<Integer>> twoSum(List<Integer> numbers, int target) {
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for (int i = 0; i < numbers.size(); i++) {
-        	// is target-current in the array?
-        	int current = numbers.get(i);
-        	boolean found = false;
+	// cache for twosum - the key is the searched value
+	Map<Integer,List<Object>> cache;
+	
+	// record a result of twosum in cache
+	void recordInCache(int target, List<List<Integer>> result) {
+		List<Object> data = new ArrayList<Object>();
+		for (List<Integer> list: result) {
+			int[] a = new int[list.size()];
+			int index = 0;
+			for (Integer i: list) {
+				a[index] = i;
+				index++;
+			}
+			data.add(a);
+		}
+		cache.put(target, data);
+	}
+	
+	// retrieve value from cache
+	List<List<Integer>> getFromCache(int target) {
+		List<Object> result = cache.get(target);
+		if (result != null) {
+			List<List<Integer>> cache = new ArrayList<List<Integer>>();
+			for (Object o: result) {
+				int[] a = (int[])o;
+				List<Integer> copy = new ArrayList<Integer>();
+				for (int i = 0; i < a.length; i++) {
+					copy.add(a[i]);
+				}
+				cache.add(copy);
+			}
+			return cache;
+		}
+		
+		return null;
+	}
+	
+	// search for 2 items in numbers adding up to target
+	// returns all matches as a list of lists (of 2 items)
+	List<List<Integer>> twoSum(int[] numbers, int target) {
+		List<List<Integer>> result = getFromCache(target);
+		if (result != null) return result;
+		result = new ArrayList<List<Integer>>();
+		
+        for (int i = 0; i < numbers.length - 1; i++) {
+			// is (target-current) in the array?
+        	int current = numbers[i];
         	int j;
-        	for (j = i + 1; j < numbers.size(); j++) {
-        		found = (numbers.get(j) == target - current);
-        		if (found) {
-        			List<Integer> list = new ArrayList<Integer>(2);
-            		list.add(numbers.get(i));
-            		list.add(numbers.get(j));
-            		if (!result.contains(list)) result.add(list);
-        		}
+    		int[] rem = new int[numbers.length - 1 - i];
+        	System.arraycopy(numbers, i+1, rem, 0, numbers.length - 1 - i);
+        	j = Arrays.binarySearch(rem, target - current);
+        	if (j >= 0) {
+        		List<Integer> list = new ArrayList<Integer>(2);
+        		list.add(numbers[i]);
+        		list.add(rem[j]);
+        		if (!result.contains(list)) result.add(list);
         	}
        }
+       
+       // record result in cache
+       recordInCache(target, result);
        
        return result; 
     }
 	
+	// given a src array and an index, build an array that does
+	// not contain the item at the given index
+	private void buildRemainder(int[] src, int i, int[] dest) {
+		// copy src[0..i-1] to dest [0..i-1]
+		// copy src[i+1..len-1] to dest [i..len-2]
+		int len = src.length;
+		System.arraycopy(src, 0, dest, 0, i);
+    	System.arraycopy(src, i + 1, dest, i, (len - 1) - ( i + 1) + 1);
+	}
+	
     public List<List<Integer>> threeSum(int[] num) {
-    	List<List<Integer>> result = new ArrayList<List<Integer>>();;
-        
-    	List<Integer> numbers = new ArrayList<Integer>();
-    	for (int i = 0; i < num.length; i++) {
-    		numbers.add(num[i]);
-    	}
+    	List<List<Integer>> result = new ArrayList<List<Integer>>();
     	
+    	// if less that 3 items, return empty list
+        if (num.length < 3) return result;
+        
+        // init cache...
+        cache = new HashMap<Integer,List<Object>>();
+        
+    	// sort numbers
+    	Arrays.sort(num);
+    	
+    	// array will collect results of twoSum for each iteration
+    	Object[] results = new Object[num.length];
+    	
+    	// remainder is num minus the current element
+    	int[] remainder = new int[num.length - 1];
     	for (int i = 0; i < num.length; i++) {
         	int a = num[i];
-        	numbers.remove(i);
-        	List<List<Integer>> match = twoSum(numbers, -a);
-        	if (!match.isEmpty()) {
-        		// we found at least 1 match
-        		for (List<Integer> list: match) {
-        			list.add(a);
-        			Collections.sort(list);
-        			if (!result.contains(list)) result.add(list);
-        		}
-        	}
-        	numbers.add(i, a);
+        	// build remainder array
+        	buildRemainder(num, i, remainder);
+        	// find -a in remainder array
+        	List<List<Integer>> match = twoSum(remainder, -a);
+        	// store result
+        	results[i] = match;
         }
         
+    	// build output list, eliminate duplicates
+    	int index = 0;
+    	for (int i = 0; i < num.length; i++) {
+    		List<List<Integer>> data = (List<List<Integer>>)results[i];
+    		if (!data.isEmpty()) {
+    			for (List<Integer> list: data) {
+    				list.add(num[index]);
+    				Collections.sort(list);
+        			if (!result.contains(list)) result.add(list);
+    			}
+    		}
+    		
+			index++;
+    	}
+    	
         return result;
     }
 }
--- test/threesum/SolutionTest.java
+++ test/threesum/SolutionTest.java
@@ -15,7 +15,7 @@ public class SolutionTest {
 
 		List<List<Integer>> result = solution.threeSum(numbers);
 		System.out.println((System.currentTimeMillis() - start) + "ms");
-
+		//System.out.println(result);
 		Assert.assertNotNull(result);
 		Assert.assertEquals("result has 1 triplet", 1, result.size());
 		Assert.assertEquals(Arrays.asList(-9, 3, 6), result.get(0));
@@ -28,7 +28,7 @@ public class SolutionTest {
 		long start = System.currentTimeMillis();
 
 		List<List<Integer>> result = solution.threeSum(numbers);
-		System.out.println(result);
+		//System.out.println(result);
 		System.out.println((System.currentTimeMillis() - start) + "ms");
 
 		Assert.assertNotNull(result);
@@ -43,7 +43,7 @@ public class SolutionTest {
 		long start = System.currentTimeMillis();
 
 		List<List<Integer>> result = solution.threeSum(numbers);
-		System.out.println(result);
+		//System.out.println(result);
 		System.out.println((System.currentTimeMillis() - start) + "ms");
 
 		Assert.assertNotNull(result);
@@ -63,9 +63,95 @@ public class SolutionTest {
 		int[] numbers = {4,-8,-9,9,10,-3,13,12,9,8,9,5,-4,-8,7,-12,-14,-11,-10,-6,2,7,-3,9,-8,9,-2,11,3,8,7,-8,-15,13,9,3,-5,-1,0,-11,-7,-5,1,4,-6,-7,-1,-13,-11,4,-4,-2,-12,0,-7,-5,-13,6,13,-3,-9,5,-4,-8,3,-10,10,5,5,-13,1,13,-11,-13,-6,-10,-4,1,-8,-8,-10,-4,6,-6,3,14,-4,5,-3,-5,9,4,-15,-9,3,-4,-4,-10,8,8,-8,-5,-2,-11};
 		long start = System.currentTimeMillis();
 		List<List<Integer>> result = solution.threeSum(numbers);
-		System.out.println(result);
+		//System.out.println(result);
+		System.out.println((System.currentTimeMillis() - start) + "ms");
+		Assert.assertNotNull(result);
+		Assert.assertEquals(118, result.size());
+		
+//		[[-15, 1, 14], [-15, 2, 13], [-15, 3, 12], [-15, 4, 11], [-15, 5, 10], 
+//		 [-15, 6, 9], [-15, 7, 8], [-14, 0, 14], [-14, 1, 13], [-14, 2, 12], 
+//		 [-14, 3, 11], [-14, 4, 10], [-14, 5, 9], [-14, 6, 8], [-14, 7, 7], 
+//		 [-13, -1, 14], [-13, 0, 13], [-13, 1, 12], [-13, 2, 11], [-13, 3, 10], 
+//		 [-13, 4, 9], [-13, 5, 8], [-13, 6, 7], [-12, -2, 14], [-12, -1, 13],
+//		 [-12, 0, 12], [-12, 1, 11], [-12, 2, 10], [-12, 3, 9], [-12, 4, 8], 
+//		 [-12, 5, 7], [-12, 6, 6], [-11, -3, 14], [-11, -2, 13], [-11, -1, 12],
+//		 [-11, 0, 11], [-11, 1, 10], [-11, 2, 9], [-11, 3, 8], [-11, 4, 7],
+//		 [-11, 5, 6], [-10, -4, 14], [-10, -3, 13], [-10, -2, 12], [-10, -1, 11],
+//		 [-10, 0, 10], [-10, 1, 9], [-10, 2, 8], [-10, 3, 7], [-10, 4, 6],
+//		 [-10, 5, 5], [-9, -5, 14], [-9, -4, 13], [-9, -3, 12], [-9, -2, 11],
+//		 [-9, -1, 10], [-9, 0, 9], [-9, 1, 8], [-9, 2, 7], [-9, 3, 6], 
+//		 [-9, 4, 5], [-8, -6, 14], [-8, -5, 13], [-8, -4, 12], [-8, -3, 11], 
+//		 [-8, -2, 10], [-8, -1, 9], [-8, 0, 8], [-8, 1, 7], [-8, 2, 6], 
+//		 [-8, 3, 5], [-8, 4, 4], [-7, -7, 14], [-7, -6, 13], [-7, -5, 12],
+//		 [-7, -4, 11], [-7, -3, 10], [-7, -2, 9], [-7, -1, 8], [-7, 0, 7], 
+//		 [-7, 1, 6], [-7, 2, 5], [-7, 3, 4], [-6, -6, 12], [-6, -5, 11],
+//		 [-6, -4, 10], [-6, -3, 9], [-6, -2, 8], [-6, -1, 7], [-6, 0, 6],
+//		 [-6, 1, 5], [-6, 2, 4], [-6, 3, 3], [-5, -5, 10], [-5, -4, 9], 
+//		 [-5, -3, 8], [-5, -2, 7], [-5, -1, 6], [-5, 0, 5], [-5, 1, 4],
+//		 [-5, 2, 3], [-4, -4, 8], [-4, -3, 7], [-4, -2, 6], [-4, -1, 5],
+//		 [-4, 0, 4], [-4, 1, 3], [-3, -3, 6], [-3, -2, 5], [-3, -1, 4],
+//		 [-3, 0, 3], [-3, 1, 2], [-2, -2, 4], [-2, -1, 3], [-2, 0, 2], 
+//		 [-2, 1, 1], [-1, -1, 2], [-1, 0, 1]]
+
+	}
+	
+	@Test
+	public void test5() {
+		Solution solution = new Solution();
+
+		int[] numbers = {};
+		long start = System.currentTimeMillis();
+
+		List<List<Integer>> result = solution.threeSum(numbers);
+		System.out.println((System.currentTimeMillis() - start) + "ms");
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals("result has 0 triplets", 0, result.size());
+	}
+	
+	@Test
+	public void test6() {
+		Solution solution = new Solution();
+
+		int[] numbers = {-7,-11,12,-15,14,4,4,11,-11,2,-8,5,8,14,0,3,2,3,-3,-15,-2,3,6,1,2,8,-5,-7,3,1,8,11,-3,6,3,-4,-13,-15,14,-8,2,-8,4,-13,13,11,5,0,0,9,-8,5,-2,14,-9,-15,-1,-6,-15,9,10,9,-2,-8,-8,-14,-5,-14,-14,-6,-15,-5,-7,5,-11,14,-7,2,-9,0,-4,-1,-9,9,-10,-11,1,-4,-2,2,-9,-15,-12,-4,-8,-5,-11,-6,-4,-9,-4,-3,-7,4,9,-2,-5,-13,7,2,-5,-12,-14,1,13,-9,-3,-9,2,3,8,0,3};
+		long start = System.currentTimeMillis();
+		List<List<Integer>> result = solution.threeSum(numbers);
+		//System.out.println(result);
 		System.out.println((System.currentTimeMillis() - start) + "ms");
 		Assert.assertNotNull(result);
 		//Assert.assertEquals("result has 6 triplets", 6, result.size());
 	}
+	
+	@Test
+	public void test7() {
+		Solution solution = new Solution();
+
+		int[] numbers = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
+		long start = System.currentTimeMillis();
+		List<List<Integer>> result = solution.threeSum(numbers);
+		//System.out.println(result);
+		System.out.println((System.currentTimeMillis() - start) + "ms");
+		Assert.assertNotNull(result);
+	}
+	
+	@Test
+	public void test8() {
+		Solution solution = new Solution();
+		int[] numbers = {0, 0};
+		List<List<Integer>> result = solution.threeSum(numbers);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(0, result.size());
+	}
+	
+	@Test
+	public void test9() {
+		Solution solution = new Solution();
+		int[] numbers = {-1,0,1,2,-1,-4};
+		List<List<Integer>> result = solution.threeSum(numbers);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(2, result.size());
+		Assert.assertTrue(result.contains(Arrays.asList(-1,-1, 2)));
+		Assert.assertTrue(result.contains(Arrays.asList(-1, 0, 1)));
+	}
+	
 }
