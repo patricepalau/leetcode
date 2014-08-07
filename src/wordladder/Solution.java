f2e2597476d7803c021113a3aa2fb8b58d3f3278
Patrice <patrice147@gmail.com>
2014-08-07 21:03:01 +0200

    solution for word ladder

--- src/wordladder/Solution.java
+++ src/wordladder/Solution.java
@@ -1,14 +1,6 @@
 package wordladder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * https://oj.leetcode.com/problems/word-ladder/
@@ -30,399 +22,159 @@ import java.util.Set;
  *   - All words have the same length.
  *   - All words contain only lowercase alphabetic characters.
  *   
- *   
- *   THIS VERSION HAS NOT BEEN ACCEPTED - TIME LIMIT EXCEEDED
+ *  The solution uses a breadth-first search.
+ *  
+ *  Note that finding the candidates is done by generating all possible words
+ *  obtained by changing 1 character on a given word (depends on the size of the word).
+ *  The alternative, i.e. evaluating words in the dictionary which are
+ *  1 letter away from a given word, depends on the size of the dictionary.
  *   
  */
 public class Solution {
-	String start;
-	String end;
-	int shortest = -1;
-	Set<String> dict;
-	List<String> words;
-	int[][] distances;
-	//int indexOfEnd;
-	//Set<String> deadends;
-	char[] endChars;
+	private LinkedList<Move> queue;
+	// dictionary
+	private Set<String>		 dict;
+	// target word
+	private String 			 end;
+	// shortest length so far
+	private int 			 shortestLength = -1;
 	
-	private boolean isSolution(List<String> moves) {
-		if (moves.size() == 0) return false;
-		String last = moves.get(moves.size() - 1);
-		return (calcDistance(endChars, last.toCharArray()) <= 1);
-	}
-	
-	private void processSolution(List<String> moves) {
-		if (shortest == -1) {
-			shortest = moves.size();
+	// data structure holds a "move"
+	// i.e. given a path so far, a word that we want to evaluate
+	// obviously we could only keep track on the search depth
+	//
+	private class Move {
+		String       word;
+		List<String> path;
+		
+		public Move(String word, List<String> path) {
+			this.word = word;
+			this.path = path;
 		}
-		else {
-			if (moves.size() < shortest) {
-				shortest = moves.size();
-			}
-		};
-	}
-	
-	Map<String,Integer> distToEnd = new HashMap<String,Integer>();
-
-long timeBuildCandidates = 0, timeSort = 0;
-int countb = 0;
-long tRemove = 0;
-	private List<String> buildCandidates(List<String> moves) {
-countb++;
-long beg = System.currentTimeMillis();
-
-		int nbMoves = moves.size();
-		String currentWord = nbMoves == 0 ? start : moves.get(moves.size() - 1);
-		
-		int currentDist = distToEnd.get(currentWord);
-
-		List<String> potentialCandidates = allPotentialCandidates.get(currentDist);
-//System.out.println("# of potentials:" + potentialCandidates.size());
-		timeSort += (System.currentTimeMillis() - beg);
-		
-		List<String> candidates = new ArrayList<String>();
-		List<String> priority = new ArrayList<String>();
-		for (String word: potentialCandidates) {
-			if (isOneCharAway(currentWord, word)) {
-				candidates.add(word);
-			}
-		}
-		
-long e = System.currentTimeMillis();
-timeBuildCandidates += (e - beg);
-		
-		return candidates;
-	}
-	
-	private Map<String, Map<String,Boolean>> cache;
-private int total = 0;
-private int hit = 0;
-	private boolean isOneCharAway(String s1, String s2) {
-total++;
-		
-		Map<String,Boolean> m1 = cache.get(s1);
-		if (m1 != null) {
-			//Boolean cached = cache[i1][i2];
-			Boolean cached = m1.get(s2);
-			if (cached != null) {
-hit++;
-				return cached;
-			}
-		}
-		
-		int dist = 0;
-
-		if (!s1.equals(s2)) {
-			char[] char1 = s1.toCharArray();
-			Arrays.sort(char1);
-			char[] char2 = s2.toCharArray();
-			
-			char[] tmp = Arrays.copyOf(char1, char1.length);
-			// how many chars in common?
-			for (int i = 0; i < char2.length; i++) {
-				int match = Arrays.binarySearch(tmp, char2[i]);
-				if (match <  0) {
-					dist++;
-				}
-				else {
-					tmp[match] = '_';
-				}
-				if (dist > 1) {
-					break;
-				}
-			}
-		}
-		if (m1 == null) {
-			m1 = new HashMap<String,Boolean>();
-			cache.put(s1, m1);
-		}
-		
-		m1.put(s2, dist <= 1);;
-		
-		return dist <= 1;
-	}
-	
-	private int calcDistance(char[] char1, char[] char2) {
-		int dist = 0;
-		char[] tmp = Arrays.copyOf(char1, char1.length);
-		// how many chars in common?
-		for (int i = 0; i < char2.length; i++) {
-			int match = Arrays.binarySearch(tmp, char2[i]);
-			if (match <  0) {
-				dist++;
-			}
-			else {
-				// remove char at match
-				// copy from 0  to match-1 and match+1 to end
-				char[] old = tmp;
-				tmp = new char[old.length - 1];
-				System.arraycopy(old, 0, tmp, 0, match);
-				if (match < old.length - 1) {
-					System.arraycopy(old, match + 1, tmp, match, old.length - 1 - match);
-				}
-			}
-		}
-		
-		return dist;
-	}
-	
-	private void makeMove(List<String> moves, String candidate) {
-		moves.add(candidate);
-		int d = distToEnd.get(candidate);
-		List<String> wsd = allPotentialCandidates.get(d);
-		wsd.remove(candidate);
-		List<String> wsd1 = null;
-		if (d + 1 < allPotentialCandidates.size()) {
-			wsd1 = allPotentialCandidates.get(d+1);
-			wsd1.remove(candidate);
+		public String toString() {
+			return path + " -> " + word;
 		}
 	}
 	
-	private void unmakeMove(List<String> moves, String candidate) {
-		moves.remove(candidate);
-		int d = distToEnd.get(candidate);
-		List<String> wsd = allPotentialCandidates.get(d);
-		if (!deadends.contains(candidate)) {
-			wsd.add(candidate);
-			if (d + 1 < allPotentialCandidates.size()) {
-				List<String> wsd1 = allPotentialCandidates.get(d+1);
-				wsd1.add(candidate);
-			}
-		}
-	}
-	
-	private void prune(List<String> moves) {	
-//		System.out.println("no candidates for moves: " + moves);
-
-		String last = moves.get(moves.size() - 1);
-		deadends.add(last);
-		int d = distToEnd.get(last);
-		List<String> ws = null;
-		if (d+1 < allPotentialCandidates.size()) {
-			ws = allPotentialCandidates.get(d+1);
-			ws.remove(last);
-		}
-		
-		ws = allPotentialCandidates.get(d);
-		ws.remove(last);
-	}
-	
-	private void backtrackDFS(List<String> moves, int k) {
-//for (int i = 0; i < k; i++) System.out.print(" ");
-//System.out.println("backtrack " + moves.size() + ": " + moves);
-		if (shortest != -1 && moves.size() >= shortest) {
-			return;
-		}
-		
-		if (isSolution(moves)) {
-			processSolution(moves);
-		}
-		else {
-			List<String> candidates = buildCandidates(moves);
-			if (candidates.isEmpty()) {
-				prune(moves);
-			}
-			
-//for (String candidate: candidates) {
-//	System.out.println("candidate: " + candidate + " -> " + distToEnd.get(candidate));
-//}
-			for (String candidate: candidates) {
-				// one of the backtracks found a solution
-				// stop here since we're not going to find a shorter sequence
-				if (shortest != -1 && moves.size() >= shortest - 1) {
-					break;
-				}
-				
-				makeMove(moves, candidate);
-				backtrackDFS(moves, k + 1);
-				unmakeMove(moves, candidate);
-			}
-		}
-	}
-	
-//	
-//	private void backtrackBFS(List<List<String>> moves, int k) {
-//		//for (int i = 0; i < k; i++) System.out.print(" ");
-//		//System.out.println("backtrack " + moves.size() + ": " + moves);
-//				if (shortest != -1 && moves.size() >= shortest) {
-//					return;
-//				}
-//				
-//				if (isSolution(moves)) {
-//					processSolution(moves);
-//					System.out.println("*** moves: " + moves);
-//				}
-//				else {
-//					List<String> candidates = buildCandidates(moves);
-//					if (candidates.isEmpty()) {
-//						prune(moves);
-//					}
-//					
-//		//for (String candidate: candidates) {
-////			System.out.println("candidate: " + candidate + " -> " + distToEnd.get(candidate));
-//		//}			
-//					List<List<String>> nextMoves = new ArrayList<List<String>>();
-//					for (String candidate: candidates) {
-//						// one of the backtracks found a solution
-//						// stop here since we're not going to find a shorter sequence
-//						if (shortest != -1 && moves.size() >= shortest - 1) {
-//							break;
-//						}
-//						
-//						makeMove(moves, candidate);
-//						nextMoves.add(new ArrayList<String>(moves));
-//						if (isSolution(moves)) {
-//							processSolution(moves);
-//						}
-//						unmakeMove(moves, candidate);
-//					}
-//					backtrackBFS(nextMoves, k + 1;)
-//				}
-//			}
-	
-	private Map<String,Integer> calculateDistancesTo(Collection<String> words, String word) {
-		Map<String,Integer> distToWord = new HashMap<String,Integer>();
-		char[]  c = word.toCharArray();
-		Arrays.sort(c);
-		
-		for (String w: words) {
-			if (w.equals(word)) {
-				continue;
-			}
-			else {
-				int d = calcDistance(c, w.toCharArray());
-				distToWord.put(w, d);
-			}
-		}
-		
-		return distToWord;
-	}
-	
-	private List<List<String>> getDistances() {
-		// abc dist max = 3 = length
-		// d=3 d=2 d=1
-		Set<String> words = new HashSet<String>(dict);
-		words.remove(end);
-		Map<String,Integer> distances = calculateDistancesTo(words, end);
-		int length = end.length() + 1;
-		List<List<String>> counts = new ArrayList<List<String>>(length);
-		for (int i = 0; i < length; i++)
-			counts.add(new ArrayList<String>());
-		
-		for (String w: distances.keySet()) {
-			int d = distances.get(w);
-			List<String> ws = counts.get(d);
-			ws.add(w);
-		}
-		
-		return counts;
-	}
-	
+	// calculates the distance from s1 to s2
 	private int calcDistance(String s1, String s2) {
-		char[]  c = s1.toCharArray();
-		Arrays.sort(c);
-		return calcDistance(c, s2.toCharArray());
+		int d = 0;
+		int len = s1.length();
+		// we compare each character holding the same position
+		// in both strings
+		for (int i = 0; i < len; i++) {
+			if (s1.charAt(i) != s2.charAt(i)) {
+				d++;
+				if (d > 1) {
+					break;
+				}
+			}
+		}
+		return d;
 	}
 	
-	private void calculateDistancesToEnd(List<String> words) {
-		for (String word: words) {
-			if (word.equals(end)) {
-				distToEnd.put(word, 0);
+	private List<String> findWords(String word, int d, List<String> ignore) {
+		List<String> result = new ArrayList<String>();; 
+		
+		// for each word, 
+		char[] letters = word.toCharArray();
+		for (int i = 0; i < word.length(); i++) {
+			// switch each letter and check whether it's in the dictionary
+			for (char c = 'a'; c <= 'z'; c++) {
+				char tmp = letters[i];
+				letters[i] = c;
+				String w = String.valueOf(letters);
+				if (dict.contains(w)) {
+					result.add(w);
+				}
+				letters[i] = tmp;
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	private void search(String word) {
+		List<String> path = new ArrayList<String>();
+		path.add(word);
+		queue.push(new Move(word, path));
+		
+		while (!queue.isEmpty()) {
+			search();
+		}
+		
+		// never got any match - return 0
+		if (shortestLength == -1) shortestLength = 0;
+	}
+	
+	private boolean isSolution(String neighbor, List<String> path) {
+		int d = calcDistance(neighbor, end);
+		return ( d <= 1 );
+	}
+	
+	private void processSolution(String neighbor, List<String> path) {	
+		int length = path.size() + 1; // adding neighbor
+		if (!neighbor.equals(end)) {
+			length++;
+		}
+		
+		if (length < shortestLength || shortestLength == -1) {
+			shortestLength = length;
+		}
+	}
+	
+	private void search() {
+		Move move = queue.pop();
+		List<String> path = move.path;
+		String word = move.word;
+		
+		// find words in dict at 1 letter distance from current or less
+		List<String> neighbors = findWords(word, 1, path);
+		// for each word
+		for (String neighbor: neighbors) {
+			if (isSolution(neighbor, path)) {
+				processSolution(neighbor, path);
 			}
 			else {
-				distToEnd.put(word, calcDistance(endChars, word.toCharArray()));
+				if (shortestLength == -1 ||
+						// current path from start + neighbor + end
+						path.size() + 2 < shortestLength) {
+					if (!path.contains(neighbor)) {
+						List<String> nextPath = new ArrayList<String>(path);
+						nextPath.add(neighbor);
+						move = new Move(neighbor, nextPath);
+						queue.add(move);
+						dict.remove(neighbor);
+					}
+				}				
 			}
 		}
+		
 	}
 	
-	private Map<String,Integer> indexes;
-	private List<String> wordsCloseToEnd;
-	List<List<String>> dist;
-	List<String> d2;
-	List<String> deadends = new ArrayList<String>();
-	List<List<String>> allPotentialCandidates = new ArrayList<List<String>>();
 	
-    public int ladderLength(String start, String end, Set<String> dict) {    
-long t = System.currentTimeMillis();
-        this.start = start;
-        this.end = end;
-        this.dict = dict;
-        this.words = new ArrayList<String>(dict);
-        this.wordsCloseToEnd = new ArrayList<String>();
-        this.words.add(start);
-        if (!words.contains(end)) this.words.add(end);
-        this.indexes = new HashMap<String,Integer>();
-        
-        cache = new HashMap<String,Map<String,Boolean>>(); //[words.size()][words.size()];
-        
-        endChars = this.end.toCharArray();
-        Arrays.sort(endChars);
-        
-long t0 = System.currentTimeMillis();
-        Collections.sort(words);
-        int index = 0;
-        for (String word: words) {
-        	indexes.put(word, index);
-        	index++;
-        }
-        
-long ta = System.currentTimeMillis();
-        this.dist = getDistances();
-        
-        // potentialCandidate[k] = dist[k] + dist[k-1]
-        //allPotentialCandidates = new ArrayList<List<String>>());
-        allPotentialCandidates.add(Arrays.asList(end));
-        for (int d = 1; d < dist.size(); d++) {
-        	List<String> potentialCandidates = new ArrayList<String>();
-        	potentialCandidates.addAll(dist.get(d-1));
-        	potentialCandidates.addAll(dist.get(d));
-        	allPotentialCandidates.add(potentialCandidates);
-        }
-        
-        int distStartToEnd =  calcDistance(start, end);
 		
-        // remove from set of candidates all words that are further away that distStartToEnd
-		for (int i = distStartToEnd + 1; i <= start.length(); i++) {
-			List<String> wordsToRemove = dist.get(i);
-			words.removeAll(wordsToRemove);
-		}
+	public int ladderLength(String start, String end, Set<String> dict) { 
+		// in case we get dummy inputs
+		if (start == null || end == null) return 0; // no match
+		if (start.equals(end))			  return 2; // direct path from start to end
 		
-long t1 = System.currentTimeMillis();
-System.out.println("trn: " + (t1-ta));
-        // pre-calculate distances from each word to the end word
-        
-        calculateDistancesToEnd(words);
-        wordsCloseToEnd = new ArrayList<String>();
-        for (String word: words) {
-        	if (distToEnd.get(word) == 1) {
-        		wordsCloseToEnd.add(word);
-        	}
-        }
-
-long t2 = System.currentTimeMillis();
-        List<String> moves = new ArrayList<String>();
-        backtrackDFS(moves, 0);
-long t3 = System.currentTimeMillis();
-        if (shortest == -1) {
-        	shortest = 0; // no path
-        }
-        else {
-        	shortest += 2; // we include start and end
-        }
-System.out.println("------------------");
-System.out.println("t0:" + (t0-t));
-System.out.println("t1:" + (t1-t0));
-System.out.println("t2:" + (t2-t1));
-System.out.println("dict:" + dict.size());
-System.out.println("t3:" + (t3-t2));
-System.out.println("tbuild:" + timeBuildCandidates);
-System.out.println("tremove:" + tRemove);
-System.out.println("countb:" + countb);
-System.out.println("tsort :" + timeSort);
-System.out.println("cache : " + hit + "/" + total);
-System.out.println("t     :" + (t3-t));
-System.out.println("------------------");
-        
-        return shortest;
-    }
+		// remember problem inputs
+		this.end   = end;
+		this.dict  = dict;
+		
+		// we're doing a breadth-first search:
+		// this queue will keep track of candidates to evaluate
+		this.queue = new LinkedList<Move>();
+		
+		// add end word to the dictionary, in case it's not there already
+		dict.add(end);
+		
+		// start breadth-first search
+		search(start);
+		
+		// this variable holds the shortest length encountered so far
+		return shortestLength;
+	}
 }
--- test/wordladder/SolutionTest.java
+++ test/wordladder/SolutionTest.java
@@ -59,9 +59,9 @@ public class SolutionTest {
 		long start = System.currentTimeMillis();
 		int result = solution.ladderLength("qa", "sq", dict);
 		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
-		Assert.assertEquals(3, result);
+		// qa ca cr sr sq
+		Assert.assertEquals(5, result);
 	}
-	
 	
 	@Test
 	public void test3() {
@@ -70,10 +70,11 @@ public class SolutionTest {
 				"kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"
 		});
 		long start = System.currentTimeMillis();
+		// cet, get, gee, gte, ate, ats, its, ito, ibo, ibm, ism = 11
 		int result = solution.ladderLength("cet", "ism", dict);
 		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
 		// cet -> set -> mes -> ism
-		Assert.assertEquals(4, result);
+		Assert.assertEquals(11, result);
 	}
 	
 	@Test
@@ -124,9 +125,10 @@ public class SolutionTest {
 				"tote","yaws","cali","mete","lula","cubs","prim","luna","jolt","span","pita","dodo","puss","deer","term","dolt","goon","gary","yarn","aims","just","rena","tine","cyst","meld","loki","wong","were","hung","maze","arid","cars","wolf","marx","faye","eave","raga","flow","neal","lone","anne","cage","tied","tilt","soto","opel","date","buns","dorm","kane","akin","ewer","drab","thai","jeer","grad","berm","rods","saki","grus","vast","late","lint","mule","risk","labs","snit","gala","find","spin","ired","slot","oafs","lies","mews","wino","milk","bout","onus","tram","jaws","peas","cleo","seat","gums","cold","vang","dewy","hood","rush","mack","yuan","odes","boos","jami","mare","plot","swab","borg","hays","form","mesh","mani","fife","good","gram","lion","myna","moor","skin","posh","burr","rime","done","ruts","pays","stem","ting","arty","slag","iron","ayes","stub","oral","gets","chid","yens","snub","ages","wide","bail","verb","lamb","bomb","army","yoke","gels","tits","bork","mils","nary","barn","hype","odom","avon","hewn","rios","cams","tact","boss","oleo","duke","eris","gwen","elms","deon","sims","quit","nest","font","dues","yeas","zeta","bevy","gent","torn","cups","worm","baum","axon","purr","vise","grew","govs","meat","chef","rest","lame"
 		});
 		long start = System.currentTimeMillis();
+		// sand, sane, sine, side, aide, aids, ands, ants, ante, anne, acne
 		int result = solution.ladderLength("sand", "acne", dict);
 		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
-		Assert.assertEquals(3, result);
+		Assert.assertEquals(11, result);
 	}
 	
 	
@@ -139,8 +141,7 @@ public class SolutionTest {
 		long start = System.currentTimeMillis();
 		int result = solution.ladderLength("nanny", "aloud", dict);
 		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
-		// danny, deann, laden, nodal
-		Assert.assertEquals(6, result);
+		Assert.assertEquals(20, result);
 	}
 	
 	@Test
@@ -152,12 +153,12 @@ public class SolutionTest {
 		long start = System.currentTimeMillis();
 		int result = solution.ladderLength("charge", "comedo", dict);
 		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
-		Assert.assertEquals(0, result);
+		Assert.assertEquals(42, result);
 	}
 	
 	@Test
 	public void test7() {
-		
+		// [nape, peak, lake, male, mile, mild]
 		Solution solution = new Solution();
 		Set<String> dict = arrayToSet(new String[]{
 			"dose","ends","dine","jars","prow","soap","guns","hops","cray","hove","ella","hour","lens","jive","wiry","earl","mara","part","flue","putt","rory","bull","york","ruts","lily","vamp","bask","peer","boat","dens","lyre","jets","wide","rile","boos","down","path","onyx","mows","toke","soto","dork","nape","mans","loin","jots","male","sits","minn","sale","pets","hugo","woke","suds","rugs","vole","warp","mite","pews","lips","pals","nigh","sulk","vice","clod","iowa","gibe","shad","carl","huns","coot","sera","mils","rose","orly","ford","void","time","eloy","risk","veep","reps","dolt","hens","tray","melt","rung","rich","saga","lust","yews","rode","many","cods","rape","last","tile","nosy","take","nope","toni","bank","jock","jody","diss","nips","bake","lima","wore","kins","cult","hart","wuss","tale","sing","lake","bogy","wigs","kari","magi","bass","pent","tost","fops","bags","duns","will","tart","drug","gale","mold","disk","spay","hows","naps","puss","gina","kara","zorn","boll","cams","boas","rave","sets","lego","hays","judy","chap","live","bahs","ohio","nibs","cuts","pups","data","kate","rump","hews","mary","stow","fang","bolt","rues","mesh","mice","rise","rant","dune","jell","laws","jove","bode","sung","nils","vila","mode","hued","cell","fies","swat","wags","nate","wist","honk","goth","told","oise","wail","tels","sore","hunk","mate","luke","tore","bond","bast","vows","ripe","fond","benz","firs","zeds","wary","baas","wins","pair","tags","cost","woes","buns","lend","bops","code","eddy","siva","oops","toed","bale","hutu","jolt","rife","darn","tape","bold","cope","cake","wisp","vats","wave","hems","bill","cord","pert","type","kroc","ucla","albs","yoko","silt","pock","drub","puny","fads","mull","pray","mole","talc","east","slay","jamb","mill","dung","jack","lynx","nome","leos","lade","sana","tike","cali","toge","pled","mile","mass","leon","sloe","lube","kans","cory","burs","race","toss","mild","tops","maze","city","sadr","bays","poet","volt","laze","gold","zuni","shea","gags","fist","ping","pope","cora","yaks","cosy","foci","plan","colo","hume","yowl","craw","pied","toga","lobs","love","lode","duds","bled","juts","gabs","fink","rock","pant","wipe","pele","suez","nina","ring","okra","warm","lyle","gape","bead","lead","jane","oink","ware","zibo","inns","mope","hang","made","fobs","gamy","fort","peak","gill","dino","dina","tier"
@@ -167,4 +168,58 @@ public class SolutionTest {
 		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
 		Assert.assertEquals(6, result);
 	}
+	
+	@Test
+	public void test8() {
+		Solution solution = new Solution();
+		Set<String> dict = arrayToSet(new String[]{"a", "b", "c"});		
+		
+		long start = System.currentTimeMillis();
+
+		int result = solution.ladderLength("a", "c", dict);
+		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
+
+		Assert.assertEquals(2, result);
+	}
+	
+	@Test
+	public void test9() {
+		Solution solution = new Solution();
+		Set<String> dict = arrayToSet(new String[]{"hot", "dog"});		
+		
+		long start = System.currentTimeMillis();
+
+		int result = solution.ladderLength("hot", "dog", dict);
+		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
+
+		Assert.assertEquals(0, result);
+	}
+	
+	@Test
+	public void test10() {
+		Solution solution = new Solution();
+		Set<String> dict = arrayToSet(new String[]{"lest","leet","lose","code","lode","robe","lost"});		
+		
+		long start = System.currentTimeMillis();
+
+		int result = solution.ladderLength("leet", "code", dict);
+		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
+
+		Assert.assertEquals(6, result);
+	}
+	
+	
+	@Test
+	public void test11() {
+		Solution solution = new Solution();
+		Set<String> dict = arrayToSet(new String[]{"talk","tons","fall","tail","gale","hall","negs"});		
+		
+		long start = System.currentTimeMillis();
+
+		int result = solution.ladderLength("talk", "tail", dict);
+		System.out.println("total: " + (System.currentTimeMillis() - start) + "ms");
+
+		Assert.assertEquals(0, result);
+	}
+
 }
