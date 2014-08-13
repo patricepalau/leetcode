84e44bb244e45e007c35704682cab1c396b131bc
Patrice <patrice147@gmail.com>
2014-08-13 20:14:00 +0200

    better solution for range search

--- src/rangesearch/Solution.java
+++ src/rangesearch/Solution.java
@@ -1,7 +1,5 @@
 package rangesearch;
 
-import java.util.Arrays;
-
 /**
  * https://oj.leetcode.com/problems/search-for-a-range/
  * 
@@ -11,30 +9,54 @@ import java.util.Arrays;
  * For example,
  * Given [5, 7, 7, 8, 8, 10] and target value 8,
  * return [3, 4].
+ * 
+ * A simple solution consists in find target + scan left and right
+ * This solution is however O(logn + p) where p is the number of occurrences.
+ * 
+ * A better solution consists in using a slightly modified binary search (see below)
+ * and searching for target-e and target+e - where e is arbitrarily small.
+ * The returned index is the index at which both values would be inserted, hence
+ * designated the first and last index of the occurrence of the target value.
+ * 
  */
 public class Solution {
+	
     public int[] searchRange(int[] A, int target) {
-    	int index = Arrays.binarySearch(A, target);
-    	int len = A.length;
+    	double epsilon = 0.1;
     	
-    	int i = -1; 
-    	int j = -1;
+    	int indexLower = binarySearch(A, 0, A.length - 1, target - epsilon);
+    	int indexUpper = binarySearch(A, 0, A.length - 1, target + epsilon);
     	
-    	if (index >= 0) {
-    		i = index;
-    		j = index;
-    		while (i >= 0 && A[i] == target) {
-        		i--;
-        	}
-        	
-        	while (j < len && A[j] == target) {
-        		j++;
-        	}
-        	
-        	i++;
-        	j--;
+    	if (indexLower == indexUpper) {
+    		// value was not found: return -1, -1 as per problem statement
+    		indexLower = -1;
+    		indexUpper = -1;
+    	}
+    	else {
+    		// indexLower points at the first occurrence of target
+    		// indexUpper points at the first element which is not target
+    		// (so possibly beyond the end of the array)
+    		indexUpper--;
     	}
     	
-    	return new int[]{i,j};
+    	return new int[]{indexLower, indexUpper};
+    }
+    
+    // binary search is modified by removing the equality condition
+    // and returning the start index instead of -1
+    private int binarySearch(int[] A, int start, int end, double target) {
+    	// not that the strict < is important here
+    	if (end < start) {
+    		return start;
+    	}
+    	
+    	int mid = (start + end) / 2;
+    	
+    	if (A[mid] > target) {
+    		return binarySearch(A, start, mid - 1, target);
+    	}
+    	else {
+    		return binarySearch(A, mid + 1, end, target);
+    	}
     }
 }
--- test/rangesearch/SolutionTest.java
+++ test/rangesearch/SolutionTest.java
@@ -24,4 +24,46 @@ public class SolutionTest {
 		int[] result = solution.searchRange(new int[]{2,2}, 3);
 		Assert.assertArrayEquals(new int[]{-1, -1}, result);
 	}
+	
+	@Test
+	public void test4() {
+		Solution solution = new Solution();
+		int[] result = solution.searchRange(new int[]{5,7,7,8,8,10}, 8);
+		Assert.assertArrayEquals(new int[]{3, 4}, result);
+	}
+	
+	@Test
+	public void test5() {
+		Solution solution = new Solution();
+		int[] result = solution.searchRange(new int[]{5,7,7,7,8,8}, 8);
+		Assert.assertArrayEquals(new int[]{4, 5}, result);
+	}
+	
+	@Test
+	public void test6() {
+		Solution solution = new Solution();
+		int[] result = solution.searchRange(new int[]{5,7,8,8,9,10}, 8);
+		Assert.assertArrayEquals(new int[]{2, 3}, result);
+	}
+	
+	@Test
+	public void test7() {
+		Solution solution = new Solution();
+		int[] result = solution.searchRange(new int[]{5,8,8,9,10,11}, 8);
+		Assert.assertArrayEquals(new int[]{1, 2}, result);
+	}
+	
+	@Test
+	public void test8() {
+		Solution solution = new Solution();
+		int[] result = solution.searchRange(new int[]{8,8,9,10,11,12}, 8);
+		Assert.assertArrayEquals(new int[]{0, 1}, result);
+	}
+	
+	@Test
+	public void test9() {
+		Solution solution = new Solution();
+		int[] result = solution.searchRange(new int[]{2,2}, 2);
+		Assert.assertArrayEquals(new int[]{0, 1}, result);
+	}
 }
