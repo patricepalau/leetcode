ba2c51efd61a68bdf09f14ec7f4c368093b1fa9a
Patrice <patrice147@gmail.com>
2014-08-22 12:42:00 +0200

    simpler solution for 3sum problem
    Not sure why my original solution was so complicated

--- src/threesum/Solution.java
+++ src/threesum/Solution.java
@@ -27,8 +27,104 @@ import java.util.Map;
  * 
  */
 public class Solution {
+	
+	public List<List<Integer>> threeSum(int [] num) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		
+		int n = num.length;
+		if (n < 3) return result;
+		
+		Arrays.sort(num);
+		
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				// is there a k in i+2...n such that a[k] = - (a[i] + a[j])
+				int value = - (num[i] + num[j]);
+				if (num[j + 1] > value) {
+					break;
+				}
+				int index = binarySearch(value, num, j + 1, n - 1);
+				if (index >= 0) {
+					// add it to results
+					List<Integer> list = new ArrayList<Integer>();
+					list.add(num[i]);
+					list.add(num[j]);
+					list.add(num[index]);
+					if (!result.contains(list)) result.add(list);
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	private int binarySearch(int value, int[] a, int start, int end) {
+		if (start > end) {
+			return -1;
+		}
+		
+		int mid = (start + end) / 2;
+		if (a[mid] == value) {
+			return mid;
+		}
+		if (a[mid] < value) {
+			return binarySearch(value, a, mid + 1, end);
+		}
+		else {
+			return binarySearch(value, a, start, mid - 1);
+		}
+	}
+	
+	
+	
 	// cache for twosum - the key is the searched value
-	Map<Integer,List<Object>> cache;
+		Map<Integer,List<Object>> cache;
+		
+		
+    public List<List<Integer>> threeSum2(int[] num) {
+    	List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	
+    	// if less that 3 items, return empty list
+        if (num.length < 3) return result;
+        
+        // init cache...
+        cache = new HashMap<Integer,List<Object>>();
+        
+    	// sort numbers
+    	Arrays.sort(num);
+    	
+    	// array will collect results of twoSum for each iteration
+    	Object[] results = new Object[num.length];
+    	
+    	// remainder is num minus the current element
+    	int[] remainder = new int[num.length - 1];
+    	for (int i = 0; i < num.length; i++) {
+        	int a = num[i];
+        	// build remainder array
+        	buildRemainder(num, i, remainder);
+        	// find -a in remainder array
+        	List<List<Integer>> match = twoSum(remainder, -a);
+        	// store result
+        	results[i] = match;
+        }
+        
+    	// build output list, eliminate duplicates
+    	int index = 0;
+    	for (int i = 0; i < num.length; i++) {
+    		List<List<Integer>> data = (List<List<Integer>>)results[i];
+    		if (!data.isEmpty()) {
+    			for (List<Integer> list: data) {
+    				list.add(num[index]);
+    				Collections.sort(list);
+        			if (!result.contains(list)) result.add(list);
+    			}
+    		}
+    		
+			index++;
+    	}
+    	
+        return result;
+    }
 	
 	// record a result of twosum in cache
 	void recordInCache(int target, List<List<Integer>> result) {
@@ -101,49 +197,4 @@ public class Solution {
 		System.arraycopy(src, 0, dest, 0, i);
     	System.arraycopy(src, i + 1, dest, i, (len - 1) - ( i + 1) + 1);
 	}
-	
-    public List<List<Integer>> threeSum(int[] num) {
-    	List<List<Integer>> result = new ArrayList<List<Integer>>();
-    	
-    	// if less that 3 items, return empty list
-        if (num.length < 3) return result;
-        
-        // init cache...
-        cache = new HashMap<Integer,List<Object>>();
-        
-    	// sort numbers
-    	Arrays.sort(num);
-    	
-    	// array will collect results of twoSum for each iteration
-    	Object[] results = new Object[num.length];
-    	
-    	// remainder is num minus the current element
-    	int[] remainder = new int[num.length - 1];
-    	for (int i = 0; i < num.length; i++) {
-        	int a = num[i];
-        	// build remainder array
-        	buildRemainder(num, i, remainder);
-        	// find -a in remainder array
-        	List<List<Integer>> match = twoSum(remainder, -a);
-        	// store result
-        	results[i] = match;
-        }
-        
-    	// build output list, eliminate duplicates
-    	int index = 0;
-    	for (int i = 0; i < num.length; i++) {
-    		List<List<Integer>> data = (List<List<Integer>>)results[i];
-    		if (!data.isEmpty()) {
-    			for (List<Integer> list: data) {
-    				list.add(num[index]);
-    				Collections.sort(list);
-        			if (!result.contains(list)) result.add(list);
-    			}
-    		}
-    		
-			index++;
-    	}
-    	
-        return result;
-    }
 }
